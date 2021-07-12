f611e40c8517a47058827dd22d1dcb1280a65a05
israfil <israfil3019@gmail.com>
2021-07-12 15:48:59 +0300

    add router , use context

---
+++ src/router/Router.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+import React from "react";
+
+
+const AppRouter = () => {
+  return (
+    <Router>
+     
+      <Switch>
+        
+      </Switch>
+      
+    </Router>
+  );
+};
+
+export default AppRouter;
